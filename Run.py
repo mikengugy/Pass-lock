ba58083aa9737acb0d59f123805e51fae85538cd
mikengugy <mikengugy@gmail.com>
2020-10-13 08:02:03 +0300

    added short codes,added functions that display the user

--- Run.py
+++ Run.py
@@ -68,4 +68,99 @@ def generate_password():
 
     return password_gen
 
+def main():
+    print("Hello Welcome to your Password locker!")
+    print('\n')
+    while True:
+        print("Use these short codes : nu - create a new user account, ln - login to your account, du - display users, et - exit password locker")
+
+        short_code = input().lower()
+
+        if short_code == "et":
+            print("Thank you for you time, Bye...")
+            break
+
+        elif short_code == "nu":
+            print("Sign Up")
+            print('-' * 30)
+            
+            user_name = input("user_name: ")
+            password = input("password: ")
+            email = input("email: ")
+
+            save_users(create_user(user_name,password,email))
+            print('\n')
+
+            print(f"Hello,{user_name} Your account was successfully been created")
+            print('\n')
+            print('-' * 30)
+
+        elif short_code == "du":
+            if display_users():
+                print("List of users:")
+                print('\n')
+                for user in display_users():
+                    print(f"{user.user_name}")
+                    print('\n')
+
+        elif short_code == "ln":
+            print("Enter your username and password to log in:")
+            print('-' * 30)
+
+            user_name = input("User name: ")
+            password = input("password: ")
+            sign_in = login_user(user_name,password)
+            if sign_in == True:
+                print(f"Hello,{user_name} select an option.")
+                while True:
+                    print('-' * 60)
+                    short_code = input("Codes: ca - create an account or name of site, dc - display the list of your credentials, ex - exit site \n").lower().strip()
+                    print('-' * 60)
+                    if short_code == "ca":
+                        print("Create new account")
+                        print('-' * 30)
+                        account_name = input("Account/site name: ")
+                        account_username = input("Site user name: ")
+                        print('-' * 60)
+                        password_option = input("Select one option: (ep-enter existing password or (gp-generate password) \n").strip()
+                        print('-' * 60)
+                        while True:
+                            if password_option == "ep":
+                                account_password = input("Enter your password: 8 characters minimum: ")
+                                break
+                            elif password_option == "gp":
+                                account_password = generate_password()
+                                break
+                            else:
+                                print("Invalid")
+                                break
+                        save_credential(create_credential(account_username,account_username,account_password))
+                        print('-' * 30)
+                        print(f"New created account: \n Account:{account_name}\n User Name:{account_username} \n Password: {account_password}")
+                        print('-' * 30)
+
+                    elif short_code == "dc":
+                        if display_credential():
+                            print("Here is your account list: ")
+
+                            print('-' * 30)
+                            for account in display_credential():
+                                print(f"Site:{account.account_name} \n User Name:{account_username} \n Password:{account_password}")
+                                print('-' * 30)
+                            print('#' * 30)
+                        else:
+                            print("Sorry, you dont have any account")
+
+                    elif short_code == "ex":
+                        print("Bye...")
+                        break
+                    else:
+                        print("Invalid")
+
+    else :
+        print("Inorder to proceed you have to create an account:")
+
+
+if __name__ == '__main__':
+    main()
 
