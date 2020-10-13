17d192cf636aae2452d73a79c0aa3a8346d05890
mikengugy <mikengugy@gmail.com>
2020-10-13 07:47:51 +0300

    class that generates new instances of credentials

--- password_locker.py
+++ password_locker.py
@@ -35,3 +35,50 @@ class User:
 
 
 
+class Credentials:
+    '''
+    class that generates new instances of credentials.
+    '''
+
+    credential_list = []
+
+    def __init__(self,account_name,account_username,account_password):
+        self.account_name = account_name
+        self.account_username = account_username
+        self.account_password = account_password
+
+    def save_credential(self):
+        '''
+        save_credential method saves credential objects into credential_list
+        '''
+        Credentials.credential_list.append(self)
+
+    def delete_credential(self):
+        '''
+        delete_credential method deletes a saved credential from the credential_list
+        '''
+
+        Credentials.credential_list.remove(self)
+
+    @classmethod
+    def display_credential(cls):
+        '''
+        method that return the credential list
+        '''
+        return cls.credential_list
+
+    @classmethod
+    def check_user_exist(cls,user_name,password):
+        '''
+        method that check if a user exist from user list
+        '''
+
+        for user in User.user_list:
+            if user.user_name == user_name and user.password == password:
+                return True
+            return False 
+    @classmethod
+    def generate_password(size = 8,char = string.ascii_letters + string.digits + string.punctuation):
+        
+        gen_pass = "".join(random.choice(char) for _ in range(8))
+        return gen_pass
